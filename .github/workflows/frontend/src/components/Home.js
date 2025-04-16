315576fb690b48ab1e3fef587e4111e110925947
Krish Ramoju <71651407+Krishramoju@users.noreply.github.com>
2025-04-16 20:17:39 +0530

    Create Home.js

---
+++ .github/workflows/frontend/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import StudentList from './StudentList';
+function Home() {
+ return (
+ <div>
+ <h1>Welcome to Student Management System</h1>
+ <StudentList />
+ </div>
+ );
+}
+export default Home;
